a06f20029dbae3702c611ba56e99e18ba83d73fe
Thibault Falise <thibault.falise@avanade.com>
2022-09-29 17:11:59 +0200

    Display list of products on catalog page

--- Sources/DevOpsShop.Web/Controllers/HomeController.cs
+++ Sources/DevOpsShop.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DevOpsShop.Web.Models;
+﻿using DevOpsShop.Model;
+using DevOpsShop.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -25,7 +26,17 @@ namespace DevOpsShop.Web.Controllers
 
         public IActionResult Catalog()
         {
-            return View (new CatalogViewModel());
+            var products = new List<Product>
+            {
+                new Product { Id = "pens-3", Name = "3 whiteboard pens", Description = "3 whiteboard pens", Price = 10m, Size = ProductSize.Small },
+                new Product { Id = "pens-8", Name = "8 whiteboard pens", Description = "8 whiteboard pens", Price = 30m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-s", Name = "Standup TShirt Size S", Description = "Standup TShirt Size S", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-m", Name = "Standup TShirt Size M", Description = "Standup TShirt Size M", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-l", Name = "Standup TShirt Size L", Description = "Standup TShirt Size L", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-xl", Name = "Standup TShirt Size XL", Description = "Standup TShirt Size XL", Price = 20m, Size = ProductSize.Small },
+            };
+
+            return View (new CatalogViewModel(products));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- Sources/DevOpsShop.Web/Models/CatalogViewModel.cs
+++ Sources/DevOpsShop.Web/Models/CatalogViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace DevOpsShop.Web.Models
+﻿using DevOpsShop.Model;
+
+namespace DevOpsShop.Web.Models
 {
     public class CatalogViewModel
     {
-        public List<string> Products { get; set; }
+        public List<Product> Products { get; set; }
+
+        public CatalogViewModel(List<Product> products)
+        {
+            Products = products;
+        }
     }
 }
