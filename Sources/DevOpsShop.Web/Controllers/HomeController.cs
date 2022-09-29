3910fd13355506a0b9d48b719d06d169eae73edd
Thibault Falise <thibault.falise@avanade.com>
2022-09-29 16:38:32 +0200

    Change index page and add catalog base page

--- Sources/DevOpsShop.Web/Controllers/HomeController.cs
+++ Sources/DevOpsShop.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace DevOpsShop.Web.Controllers
             return View();
         }
 
+        public IActionResult Catalog()
+        {
+            return View (new CatalogViewModel());
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Sources/DevOpsShop.Web/Models/CatalogViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DevOpsShop.Web.Models
+{
+    public class CatalogViewModel
+    {
+        public List<string> Products { get; set; }
+    }
+}
