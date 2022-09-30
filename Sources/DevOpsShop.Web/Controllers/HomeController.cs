9ac533030f93a966942509d492127db7e53ab2b7
Thibault Falise <thibault.falise@avanade.com>
2022-09-30 10:44:52 +0200

    Add product view

--- Sources/DevOpsShop.Web/Controllers/HomeController.cs
+++ Sources/DevOpsShop.Web/Controllers/HomeController.cs
@@ -8,12 +8,22 @@ namespace DevOpsShop.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly List<Product> _products = new List<Product>
+            {
+                new Product { Id = "pens-3", Name = "3 whiteboard pens", Description = "3 whiteboard pens", Price = 10m, Size = ProductSize.Small },
+                new Product { Id = "pens-8", Name = "8 whiteboard pens", Description = "8 whiteboard pens", Price = 30m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-s", Name = "Standup TShirt Size S", Description = "Standup TShirt Size S", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-m", Name = "Standup TShirt Size M", Description = "Standup TShirt Size M", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-l", Name = "Standup TShirt Size L", Description = "Standup TShirt Size L", Price = 20m, Size = ProductSize.Small },
+                new Product { Id = "tshirt-xl", Name = "Standup TShirt Size XL", Description = "Standup TShirt Size XL", Price = 20m, Size = ProductSize.Small },
+            };
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        [Route("")]
         public IActionResult Index()
         {
             return View();
@@ -24,19 +34,23 @@ namespace DevOpsShop.Web.Controllers
             return View();
         }
 
+        [Route("catalog")]
         public IActionResult Catalog()
         {
-            var products = new List<Product>
-            {
-                new Product { Id = "pens-3", Name = "3 whiteboard pens", Description = "3 whiteboard pens", Price = 10m, Size = ProductSize.Small },
-                new Product { Id = "pens-8", Name = "8 whiteboard pens", Description = "8 whiteboard pens", Price = 30m, Size = ProductSize.Small },
-                new Product { Id = "tshirt-s", Name = "Standup TShirt Size S", Description = "Standup TShirt Size S", Price = 20m, Size = ProductSize.Small },
-                new Product { Id = "tshirt-m", Name = "Standup TShirt Size M", Description = "Standup TShirt Size M", Price = 20m, Size = ProductSize.Small },
-                new Product { Id = "tshirt-l", Name = "Standup TShirt Size L", Description = "Standup TShirt Size L", Price = 20m, Size = ProductSize.Small },
-                new Product { Id = "tshirt-xl", Name = "Standup TShirt Size XL", Description = "Standup TShirt Size XL", Price = 20m, Size = ProductSize.Small },
-            };
+            return View (new CatalogViewModel(_products));
+        }
 
-            return View (new CatalogViewModel(products));
+        [Route("product/{productId}")]
+        public IActionResult Product(string productId)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == productId);
+
+            if (product == null)
+            {
+                return View("Error");
+            }
+
+            return View(new ProductViewModel(product));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Sources/DevOpsShop.Web/Models/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DevOpsShop.Model;
+
+namespace DevOpsShop.Web.Models
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; }
+
+        public ProductViewModel(Product product)
+        {
+            Product = product;
+        }
+    }
+}
